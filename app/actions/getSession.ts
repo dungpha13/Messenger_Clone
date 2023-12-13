c4f0249ffb6b1a1f2835b34d459cea1adb5e6894
handgun <dungphase@gmail.com>
2023-12-13 20:37:05 +0700

    fix deploy 3

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../libs/authOptions";
 
 export default async function getSession() {
     return await getServerSession(authOptions)
